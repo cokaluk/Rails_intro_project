3175cfd77e40a18f2bf3362675b7e085fc64a02f
cokaluk <cokaluk@academic.rrc.ca>
2024-06-28 10:13:01 -0500

    added active_admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,9 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
