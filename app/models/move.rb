bcb81eed992fac0c0d8fb1f4861bdb67795c0de3
cokaluk <cokaluk@academic.rrc.ca>
2024-07-04 14:11:50 -0500

    added validations to all models

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,5 @@
 class Move < ApplicationRecord
   has_and_belongs_to_many :pokemons
+
+  validates :name, presence: true
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,8 @@
 class Pokemon < ApplicationRecord
   has_and_belongs_to_many :types
   has_and_belongs_to_many :moves
+
+  validates :name, presence: true
+  validates :height, numericality: { greater_than: 0 }
+  validates :weight, numericality: { greater_than: 0 }
 end
