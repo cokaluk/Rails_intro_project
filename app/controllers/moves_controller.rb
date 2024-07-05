ae7f1d027c82fcb10633ea707ab39913f68d8953
cokaluk <cokaluk@academic.rrc.ca>
2024-07-05 08:54:59 -0500

    added moves and types index/show pages

---
+++ app/controllers/moves_controller.rb
@@ -0,0 +1,9 @@
+class MovesController < ApplicationController
+  def index
+    @moves = Move.all
+  end
+
+  def show
+    @move = Move.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
 
   get "/about", to: "application#about"
 
-  resources :pokemon, only: [:show]
+  resources :pokemon, only: [:index, :show]
+
+  resources :types, only: [:index, :show]
+
+  resources :moves, only: [:index, :show]
 end
